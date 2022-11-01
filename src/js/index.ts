fd25b312f1f0aa108c189d3f0223e46eebc11929
Kristján Oddsson <koddsson@gmail.com>
2022-11-01 22:58:26 +0100

    Implement different colors

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,10 +4,17 @@ const ctx = canvas.getContext('2d')!
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-function createCircle(x: number, y: number, radius = 70) {
+interface Circle {
+  color: string
+}
+
+const circles: Record<number, Circle> = {}
+const colors = ['yellow', 'blue', 'red', 'green']
+
+function drawCircle(x: number, y: number, color: string, radius = 70) {
   ctx.beginPath()
   ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
-  ctx.fillStyle = 'green'
+  ctx.fillStyle = color
   ctx.fill()
   ctx.lineWidth = 5
   ctx.strokeStyle = '#003300'
@@ -15,9 +22,23 @@ function createCircle(x: number, y: number, radius = 70) {
 }
 
 function draw(event: TouchEvent) {
+  if (event.type === 'touchstart') {
+    for (const touch of event.changedTouches) {
+      circles[touch.identifier] = {
+        color: colors.pop()!
+      }
+    }
+  }
+  if (event.type === 'touchend' || event.type === 'touchcancel') {
+    for (const touch of event.changedTouches) {
+      const circle = circles[touch.identifier]
+      colors.push(circle.color)
+      delete circles[touch.identifier]
+    }
+  }
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   for (const touch of event.touches) {
-    createCircle(touch.clientX, touch.clientY)
+    drawCircle(touch.clientX, touch.clientY, circles[touch.identifier].color)
   }
 }
 
