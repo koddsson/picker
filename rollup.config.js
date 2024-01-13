6626bd44ecd0e452c23a66f346adbc09a9063c0e
Kristján Oddsson <koddsson@gmail.com>
2024-01-13 17:04:12 +0100

    Fix rollup plugin import

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import html from '@web/rollup-plugin-html'
+import {rollupPluginHTML as html} from '@web/rollup-plugin-html'
 import esbuild from 'rollup-plugin-esbuild'
 
 export default {
   input: 'index.html',
   output: {dir: 'dist'},
-  plugins: [esbuild(), html({input: 'index.html'})]
+  plugins: [esbuild(), html({input: 'index.html'})],
 }
